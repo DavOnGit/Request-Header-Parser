23c4ce9c2da41b1ad81a88acb6569adff6cdd809
Daviz <davide.vozzi@gmail.com>
2016-09-16 13:35:57 +0000

    add time field to response

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Request Header Parser Microservice
 
+This is a simple App that parse every request header.
+
+To make a request and see it in action just [Click Me! :)](https://timestamp-microservice-pantarei.c9users.io/api/hparser)
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) installed.
+
+```sh
+$ git clone https://github.com/DavOnGit/Request-Header-Parser.git # or clone your own fork
+$ cd node-mongo-heroku-getting-started
+$ npm install
+$ npm start
+```
+
+Your app should now be running on [localhost:5000](http://localhost:5000/).
+
+## Deploying to Heroku
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+or
+
+Make sure you have the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/api/hparser', function(req,res) {
-    
+    var loclan = req.headers['accept-language'].match(/^[a-z]{2}/)[0];
     var cli = {
-        ipaddress: req.ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/).toString(),
-        language: req.headers['accept-language'].match(/^[a-z]{2}-[A-Z]{2}/).toString(),
-        software: req.headers['user-agent'].replace(/^.+\(/,'').replace(/\).+$/,'').replace(/\srv:\d+\.\d+$/,'')
+        ipaddress: req.ip.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)[0],
+        language: req.headers['accept-language'].match(/^[a-z]{2}-[A-Z]{2}/)[0],
+        software: req.headers['user-agent'].replace(/^.+\(/,'').replace(/\).+$/,'').replace(/\srv:\d+\.\d+$/,''),
+        time: moment().locale(loclan).format('HH:mm:ss - LL'),
     };
     res.send(cli);
 });
